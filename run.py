84c5067a4de345da4dbd3326f1562d7134a63b73
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-06-21 15:36:56 +0000

    Put the game flow into functions

--- run.py
+++ run.py
@@ -1,42 +1,82 @@
-name = input('Enter your name!')
-print(f'Hi {name}')
-start = input('would you rather play the game')
-if start == 'play':
-    print('great')
-    setting = input('stairs or elevator?')
-else:
-    print('lame')
-    quit()
+import time
 
-if setting == 'stairs':
+
+def intro():
+    print('intoducing text')
+    time.sleep(1)
+    print('stairs or elevator')
+    choice = input()
+    if choice == 'stairs':
+        option_stairs()
+    elif choice == 'elevator':
+        option_elevator()
+    else:
+        print('Type stairs or elevator please!')
+        intro()
+
+
+def option_stairs():
+    print('intoducing text stairs')
+    time.sleep(1)
     print('distinguish or lather')
-    response = input()
-    if response == 'distinguish':
-        print('window or hole')
-        response_second = input( )
-        if response_second == 'hole':
-            print('You survived')
-        elif response_second == 'window':
-            print('you died') 
-    elif response == 'lather':
-        print('you died')
-
-elif setting == 'elevator':
-    print('lather of hole')
-    choose = input()
-    if choose == 'lather':
-        print('window or hole')
-        choose_second = input()
-        if choose_second == 'window':
-            print('you died')
-        elif choose_second == 'hole':
-            print('you survived')
-    elif choose == 'hole':
-        print('lather or jump?')
-        option = input( )
-        if option == 'lather':
-            print('you died')
-        elif option == 'jump':
-            print('you survived!')
+    choice = input()
+    if choice == 'distinguish':
+        option_distinguish_lather()
+    elif choice == 'lather':
+        dead()
+    else:
+        print('Type distinguish or lather please!')
+        option_stairs()
 
 
+def option_elevator():
+    print('intoducing text elevator')
+    time.sleep(1)
+    print('lather or hole?')
+    choice = input()
+    if choice == 'lather':
+        option_distinguish_lather()
+    elif choice == 'hole':
+        option_hole()
+    else:
+        print('Type lather or hole please!')
+        option_elevator()
+
+
+def option_distinguish_lather():
+    print('introduction distinguish')
+    time.sleep(1)
+    print('window or hole?')
+    choice = input()
+    if choice == 'window':
+        dead()
+    elif choice == 'hole':
+        survive()
+    else:
+        print('Type lather or hole please!')
+        option_distinguish_lather()
+
+
+def option_hole():
+    print('intro hole')
+    time.sleep(1)
+    print('dynamite or pole?')
+    choice = input()
+    if choice == 'dynamite':
+        survive()
+    elif choice == 'pole':
+        dead()
+    else:
+        print('Type lather or hole please!')
+        option_hole()
+
+
+def dead():
+    print('Game Over')
+
+
+def survive():
+    print('congratulations you made it!')
+
+
+intro()

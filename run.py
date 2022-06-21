e680c9426750c56a6e54ac3a04c09286126a4f1c
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-06-21 14:38:02 +0000

    rearranged main structure and add a flow of if statements

--- run.py
+++ run.py
@@ -1,44 +1,42 @@
-def scene1():
-    """
-    This is a text based adventure game. The story continue as the user makes different choices along the way. In this first scene the 
-    user gets a background story and what the mission is. After that the user got to decide if using the stairs or elevator to get out
-    from the building.
-    """
+name = input('Enter your name!')
+print(f'Hi {name}')
+start = input('would you rather play the game')
+if start == 'play':
+    print('great')
+    setting = input('stairs or elevator?')
+else:
+    print('lame')
+    quit()
 
-    import time
-    print("""WELCOME TO ESCAPE THE BUILDING!
-    Let's get started with this text based adventure experience! 
-    
-    Peter is a tourist in London and is taking part of a tour in
-    a beutiful museum about the history on England. They are at the top floor
-    and the guid is talking about many intresting facts about the history of architecture of London.
-    AND THEN!!! They hear two big explosions. BAAM! BAAM! 
-    The tourguid reises the valume of the radio. They got informed that two bombs has exploded on the 
-    first and secound floor and it accured a fire. I look behind me and I see another bomb ticking.
-    
-    Your mission is to get out from the building alive before the bomb explode!
-    """)
-    print('You see two possiblities, either you take the elvator or the stairs')
-    print('Type your choice: elevator or staris?')
+if setting == 'stairs':
+    print('distinguish or lather')
+    response = input()
+    if response == 'distinguish':
+        print('window or hole')
+        response_second = input( )
+        if response_second == 'hole':
+            print('You survived')
+        elif response_second == 'window':
+            print('you died') 
+    elif response == 'lather':
+        print('you died')
 
-    first_scene_input = input()
-    time.sleep(2)
-    ans = 'incorrect'
-    while(ans =='incorrect'):
-        if(first_scene_input.upper() == 'STAIRS'):
-            print('you chosed stairs')
-            ans = 'correct'
-            stairs('You decided to take the stairs')
-        elif(first_scene_input.upper() == 'ELEVATOR'):
-            print('you choosed elevator')
-            ans = 'correct'
-            elevator('You decided to take the elevator')
-        else:
-            print('PLEASE ENTER ONE OF THE CHOICES: Elevator or stairs')
-            first_scene_input = input()
-scene1()
-
-
-
+elif setting == 'elevator':
+    print('lather of hole')
+    choose = input()
+    if choose == 'lather':
+        print('window or hole')
+        choose_second = input()
+        if choose_second == 'window':
+            print('you died')
+        elif choose_second == 'hole':
+            print('you survived')
+    elif choose == 'hole':
+        print('lather or jump?')
+        option = input( )
+        if option == 'lather':
+            print('you died')
+        elif option == 'jump':
+            print('you survived!')
 
 
